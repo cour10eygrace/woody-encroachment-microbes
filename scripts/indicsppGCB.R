2b2c84dc91966a37e5e947e66f6df27ce7ce57da
cour10eygrace <courtneygrace125@gmail.com>
2020-08-27 20:40:57 -0600

    finalize scripts

--- scripts/indicsppGCB.R
+++ scripts/indicsppGCB.R
@@ -3,27 +3,24 @@ library(permute)
 library(indicspecies)
 
 #FUNGI
-#load otu table, .csv, remove #created from biom file first line, and remove #otu_id cell
-otu<- read.csv("CCGlobalProject.otu_table.taxonomy.csv")
-rownames(otu)<-otu$ï..OTU.ID
-otu$ï..OTU.ID<-NULL
+#load otu table, 
+otu<- read.csv("data/CCGlobalProject.otu_table.taxonomy.csv")
+rownames(otu)<-otu$OTU_ID
+otu$OTU_ID<-NULL
 otu$Taxonomy<-NULL 
 otu$ITS.cc.NTC<-NULL ##no template control
 
-#load mapping file, remove 'sample ID' cell before
-map = read.csv("Alldata.csv" , header=T, row.names=43)
-map = map[ ,c(4,8,10,18)]
+#load mapping file,
+map = read.csv("data/soil.data.csv" , header=T, row.names=12)
 
 #make dataframe for taxa
-taxa<- read.csv("CCGlobalProject.taxonomy.fix.csv", row.names = 1)
+taxa<- read.csv("data/CCGlobalProject.taxonomy.fix.csv", row.names = 1)
 taxa<-subset(taxa, Kingdom=="Fungi")
 taxa<-as.data.frame(taxa)
 
 #transpose OTU table so that samples are rows and OTU IDs are columns
 OTU.transp<-t(otu)
 OTU.transp<-as.data.frame(OTU.transp)
-str(OTU.transp)
-head(OTU.transp)
 
 #match order of mapping file with order of samples in OTU table
 p<-match(row.names(OTU.transp),row.names(map))
@@ -33,8 +30,8 @@ map2<-na.omit(map2)
 #check
 nrow(map2)==nrow(OTU.transp)
 
-#create factor name to call for Uniqueclass, i.e., unique tree by site combination
-Veg<-as.factor(map2$Vegetation)
+#create factor name to call for Uniqueclass
+Veg<-as.factor(map2$veg)
 
 #indicator species analysis by unique identifier
 indval_veg<-multipatt(x=as.data.frame(OTU.transp),
@@ -47,31 +44,29 @@ indval_veg<-multipatt(x=as.data.frame(OTU.transp),
 summary.multipatt(indval_veg, minstat = 0.3)#stat=0.3 means taxa is found in 30% or more of the samples in that group  
 
 itsveg<-as.data.frame(indval_veg$sign)%>%
-  #subset(., p.value<0.05&stat>0.1)%>%
+  subset(., p.value<0.05&stat>0.19)%>%
   merge(.,taxa,by='row.names')%>%
   subset(., Species!="")
 
 
 #BACTERIA
-#load otu table, .csv, remove #created from biom file first line, and remove #otu_id cell
-otu_b<-read.csv("feature-table_Cfilt.csv")  
+#load otu table-filtered  
+otu_b<-read.csv("data/feature-table_Cfilt.csv")  
 rownames(otu_b)<-otu_b$Feature.ID
 otu_b$Feature.ID<-NULL
 otu_b$ITS.cc.NTC1<-NULL ##no template control
 otu_b$ITS.cc.NTC2<-NULL ##no template control
 
 #make dataframe for taxa
-taxa_b<-read.csv("taxonomy.csv")  
+taxa_b<-read.csv("data/taxonomy.csv")  
 taxa_b<-subset(taxa_b, Kingdom=="Bacteria")
 taxa_b<-as.data.frame(taxa_b)
-rownames(taxa_b)<-taxa_b$ï..
-taxa_b$ï..<-NULL
+rownames(taxa_b)<-taxa_b$ID
+taxa_b$ID<-NULL
 
 #transpose OTU table so that samples are rows and OTU IDs are columns
 OTU.transp_b<-t(otu_b)
 OTU.transp_b<-as.data.frame(OTU.transp_b)
-str(OTU.transp_b)
-head(OTU.transp_b)
 
 #match order of mapping file with order of samples in OTU table
 pb<-match(row.names(OTU.transp_b),row.names(map))
@@ -82,7 +77,7 @@ map2b<-na.omit(map2b)
 nrow(map2b)==nrow(OTU.transp_b)
 
 #create factor name to call for Uniqueclass, i.e., unique tree by site combination
-Veg<-as.factor(map2b$Vegetation)
+Veg<-as.factor(map2b$veg)
 
 #indicator species analysis by unique identifier
 indval_veg_b<-multipatt(x=as.data.frame(OTU.transp_b),
@@ -94,7 +89,7 @@ indval_veg_b<-multipatt(x=as.data.frame(OTU.transp_b),
 summary.multipatt(indval_veg_b, minstat = 0.3)#stat=0.3 means taxa is found in 30% or more of the samples in that group  
 
 X16sveg<-as.data.frame(indval_veg_b$sign)%>%
-  subset(., p.value<0.05&stat>0.2)%>%
+  subset(., p.value<0.05&stat>0.19)%>%
   merge(.,taxa_b,by='row.names')%>%
   subset(., Genus!="")
 
--- scripts/taxaabundGCB.R
+++ scripts/taxaabundGCB.R
@@ -3,32 +3,28 @@ library(reshape2)
 library(lme4)
 #FUNGI 
 #read in otu table 
-otus <- read.csv("CCGlobalProject.otu_table.taxonomy.csv")
-otus$OTUID<-otus$ï..OTU.ID
-otus$ï..OTU.ID<-NULL
+otus <- read.csv("data/CCGlobalProject.otu_table.taxonomy.csv")
+otus$OTUID<-otus$OTU_ID
+otus$OTU_ID<-NULL
 otus$Taxonomy<-NULL
 otus$ITS.cc.NTC<-NULL
 
 #read in updated taxonomy
-taxmat <- read.csv("CCGlobalProject.taxonomy.fix.csv")
+taxmat <- read.csv("data/CCGlobalProject.taxonomy.fix.csv")
 taxmat<-subset(taxmat, Kingdom=="Fungi")
-taxmat$OTUID<-taxmat$ï..
-taxmat$ï..<-NULL
 
 #merge
 otu_tax<-merge(taxmat, otus, by="OTUID")
 
 #read in metadata 
-map = read.csv("Alldata.csv" , header=T, row.names=43)
-map = map[ ,c(42,4,18)]
-map$Sample.ID<-row.names(map)
+map = read.csv("data/soil.data.csv" , header=T)
 
 #make otu table long 
 otul <- melt(otu_tax)
-names(otul)[names(otul)=="variable"] <-"Sample.ID"
+names(otul)[names(otul)=="variable"] <-"SampleID"
 names(otul)[names(otul)=="value"] <- "itsreads"
 #merge with metadata 
-otu_all<-merge(otul,map, by="Sample.ID")
+otu_all<-merge(otul,map, by="SampleID")
 
 #filter out zero reads 
 itsl_reads<-subset(otu_all, itsreads>0)
@@ -44,100 +40,92 @@ mort<-subset(itsl_reads, Phylum=="Mortierellomycota")
 
 ##run mixed effects models 
 hist(asco$itsreads)# not normal 
-hist(log(asco$itsreads))#still not normal 
-asco_veg<-glmer((log(itsreads)+1)~Vegetation + (1|Location), asco, family = Gamma) 
-summary(asco_veg)#S<NS (p=0.08)
+hist(log(asco$itsreads))#not normal 
+asco_veg<-glmer((log(itsreads)+1)~veg + (1|site), asco, family = Gamma) 
+summary(asco_veg)#S<NS 
 
 hist(basidio$itsreads)#not normal 
-hist(log(basidio$itsreads))#still not normal 
-basidio_veg<-glmer(log(itsreads)+1~Vegetation + (1|Location), basidio, family = Gamma) 
+hist(log(basidio$itsreads))#not normal 
+basidio_veg<-glmer(log(itsreads)+1~veg + (1|site), basidio, family = Gamma) 
 summary(basidio_veg)#S<NS
-plot(log(itsreads)~Vegetation, basidio)
 
 hist(chyt$itsreads)#not normal
-hist(log(chyt$itsreads))#still not normal 
-chyt_veg<-glmer((log(itsreads)+1)~Vegetation + (1|Location), chyt, family=Gamma)
+hist(log(chyt$itsreads))#not normal 
+chyt_veg<-glmer((log(itsreads)+1)~veg + (1|site), chyt, family=Gamma)
 summary(chyt_veg)#NS
 
 hist(glom$itsreads)#not normal
-hist(log(glom$itsreads))#still not normal 
-glom_veg<-glmer((log(itsreads)+1)~Vegetation + (1|Location), glom, family=Gamma)
+hist(log(glom$itsreads))#not normal 
+glom_veg<-glmer((log(itsreads)+1)~veg + (1|site), glom, family=Gamma)
 summary(glom_veg)#NS
 
 hist(mort$itsreads)#not normal
-hist(log(mort$itsreads))#still not normal 
-mort_veg<-glmer((log(itsreads)+1)~Vegetation + (1|Location), mort, family=Gamma)
+hist(log(mort$itsreads))#not normal 
+mort_veg<-glmer((log(itsreads)+1)~veg + (1|site), mort, family=Gamma)
 summary(mort_veg)#NS
 
 #TAXA---- 
 #read in otu table
-otus_b <- read.csv("feature-table_Cfilt.csv")
+otus_b <- read.csv("data/feature-table_Cfilt.csv")
 otus_b$ITS.cc.NTC1<-NULL
 otus_b$ITS.cc.NTC2<-NULL
 #read in taxonomy
-taxmat_b<- read.csv("taxonomy.csv")
+taxmat_b<- read.csv("data/taxonomy.csv")
 taxmat_b<-subset(taxmat_b, Kingdom=="Bacteria")
-taxmat_b$Feature.ID<-taxmat_b$ï..
-taxmat_b$ï..<-NULL
+taxmat_b$Feature.ID<-taxmat_b$ID
+taxmat_b$ID<-NULL
 otu_tax_b<-merge(otus_b, taxmat_b, by="Feature.ID")
 #make otu table long 
 otulb <- melt(otu_tax_b)
-names(otulb)[names(otulb)=="variable"] <- "Sample.ID"
+names(otulb)[names(otulb)=="variable"] <- "SampleID"
 names(otulb)[names(otulb)=="value"] <- "X16Sreads"
 #merge with metadata
-otu_all_b<-merge(otulb,map, by="Sample.ID")
+otu_all_b<-merge(otulb,map, by="SampleID")
 #filter out zero reads 
 otulb_reads<-subset(otu_all_b, X16Sreads>0)
 
 #subset by phylum 
-acido<-subset(otulb_reads, Phylum=="Acidobacteria")#NS>S
-actino<-subset(otulb_reads, Phylum=="Actinobacteria")#NS>S
-proteo<-subset(otulb_reads, Phylum=="Proteobacteria")#NS>S
-firm<-subset(otulb_reads, Phylum=="Firmicutes")#S>NS
-very<-subset(otulb_reads, Phylum=="Verrucomicrobia")#NS>S
-bact<-subset(otulb_reads, Phylum=="Bacteroidetes")#S>NS
-planct<-subset(otulb_reads, Phylum=="Planctomycetes")#NS>S
-chlor<-subset(otulb_reads, Phylum=="Chloroflexi")#not sig
+acido<-subset(otulb_reads, Phylum=="Acidobacteria")
+actino<-subset(otulb_reads, Phylum=="Actinobacteria")
+proteo<-subset(otulb_reads, Phylum=="Proteobacteria")
+firm<-subset(otulb_reads, Phylum=="Firmicutes")
+very<-subset(otulb_reads, Phylum=="Verrucomicrobia")
+bact<-subset(otulb_reads, Phylum=="Bacteroidetes")
+planct<-subset(otulb_reads, Phylum=="Planctomycetes")
+chlor<-subset(otulb_reads, Phylum=="Chloroflexi")
 
 
 hist(log(acido$X16Sreads))#normal 
-acido_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), acido) 
-summary(acido_veg)#sig
-plot(log(X16Sreads)~Vegetation, acido)
+acido_veg<-lmer(log(X16Sreads)~veg + (1|site), acido) 
+summary(acido_veg)
 
 hist(log(actino$X16Sreads))#normal 
-actino_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), actino) 
-summary(actino_veg)#sig
-plot(log(X16Sreads)~Vegetation, actino)
+actino_veg<-lmer(log(X16Sreads)~veg + (1|site), actino) 
+summary(actino_veg)
 
 hist(log(proteo$X16Sreads))#normal 
-proteo_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), proteo) 
-summary(proteo_veg)#sig
-plot(log(X16Sreads)~Vegetation, proteo)
+proteo_veg<-lmer(log(X16Sreads)~veg + (1|site), proteo) 
+summary(proteo_veg)
 
 hist(log(firm$X16Sreads))#normal 
-firm_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), firm) 
-summary(firm_veg)#sig (p=0.075)
-plot(log(X16Sreads)~Vegetation, firm)
+firm_veg<-lmer(log(X16Sreads)~veg + (1|site), firm) 
+summary(firm_veg)
 
 hist(log(very$X16Sreads))#normal 
-ver_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), very) 
-summary(ver_veg)#sig
-plot(log(X16Sreads)~Vegetation, very)
+ver_veg<-lmer(log(X16Sreads)~veg + (1|site), very) 
+summary(ver_veg)
 
 hist(log(chlor$X16Sreads))#normal 
-chlor_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), chlor) 
-summary(chlor_veg)#NS
+chlor_veg<-lmer(log(X16Sreads)~veg + (1|site), chlor) 
+summary(chlor_veg)
 
 hist(log(bact$X16Sreads))#normal 
-bact_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), bact) 
-summary(bact_veg) #sig (p=0.056)
-plot(log(X16Sreads)~Vegetation, bact)
+bact_veg<-lmer(log(X16Sreads)~veg + (1|site), bact) 
+summary(bact_veg) 
 
 hist(log(planct$X16Sreads))#normal 
-planct_veg<-lmer(log(X16Sreads)~Vegetation + (1|Location), planct) 
-summary(planct_veg)#sig
-plot(log(X16Sreads)~Vegetation, planct)
+planct_veg<-lmer(log(X16Sreads)~veg + (1|site), planct) 
+summary(planct_veg)
 
 
 
